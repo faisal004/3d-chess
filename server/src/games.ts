3f32c4864d145b1a50ef4c6f3bbe5ea63e95041c
Faisal Husain <faisalhusain1320@gmail.com>
2025-06-14 22:02:57 +0530

    add : basic backed is done

---
+++ server/src/games.ts
@@ -0,0 +1,19 @@
+import { Chess } from "chess.js";
+import { WebSocket } from "ws";
+
+export class Game {
+    public player1: WebSocket;
+    public player2: WebSocket;
+    private board: Chess;
+    private startTime: Date;
+    constructor(player1: WebSocket, player2: WebSocket) {
+        this.player1 = player1;
+        this.player2 = player2;
+        this.board = new Chess()
+        this.startTime = new Date();
+    }
+
+    makeMove(socket:WebSocket, move:string){
+        
+    }
+}
