1316a0f6140f1abf9d7acf97c11f848b0419fc75
Faisal Husain <faisalhusain1320@gmail.com>
2025-06-15 16:38:44 +0530

    add : basis chess board added

--- frontend/src/components/3d/experience.tsx
+++ frontend/src/components/3d/experience.tsx
@@ -3,29 +3,34 @@
 import { OrbitControls } from '@react-three/drei'
 import Lights from './lights'
 
-const  Experience = () =>
-{
+const Experience = () => {
     return <>
-
         <OrbitControls makeDefault />
-
         <Lights />
-
-        <mesh castShadow position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
+        <mesh receiveShadow position={[0, -0.16, 0]} >
+            <boxGeometry args={[8.8, 0.3, 8.8]} />
+            <meshStandardMaterial color="#333" />
         </mesh>
-
-        <mesh castShadow position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
-
-        <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
-
+        {Array.from({ length: 8 }).map((_, row) =>
+            Array.from({ length: 8 }).map((_, col) => {
+                const isWhite = (row + col) % 2 === 1;
+                return (
+                    <mesh
+                        key={`square-${row}-${col}`}
+                        position={[
+                            col - 3.5,
+                            0,
+                            row - 3.5
+                        ]}
+                        receiveShadow
+                        castShadow
+                    >
+                        <boxGeometry args={[1, 0.1, 1]} />
+                        <meshStandardMaterial color={isWhite ? '#ffffff' : '#000000'} />
+                    </mesh>
+                );
+            })
+        )}
     </>
 }
 
