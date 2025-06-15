7b83d2e9706ec9d0d74068e1ee3e628790e3d579
Faisal Husain <faisalhusain1320@gmail.com>
2025-06-15 17:23:37 +0530

    revert : some changes dont work

--- frontend/src/pages/testPage.tsx
+++ frontend/src/pages/testPage.tsx
@@ -5,8 +5,16 @@ const TestPage: React.FC = () => {
   return (
     <Canvas
     shadows
-    className="bg-linear-to-b from-black to-zinc-700 fixed top-0 left-0 h-[100%] w-[100%]"
+    className="bg-linear-to-b from-black to-zinc-700 "
     camera={{ position: [10, 10, 10], fov: 50 }}
+
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+      }}
     >
       <Experience />
     </Canvas>
