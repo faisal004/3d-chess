695365b73f2497e37995de7f03cee53465366e01
Faisal Husain <faisalhusain1320@gmail.com>
2025-06-15 21:17:23 +0530

    clean : code clean

--- frontend/src/pages/GamePage.tsx
+++ frontend/src/pages/GamePage.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@/components/ui/button";
 import { useSocket } from "@/hooks/useSocket";
 import { INIT_GAME, OPPONENT_LEFT } from "@/types/socket";
 import React, { useEffect, useState, useCallback } from "react";
 import ChessBoard from "@/components/3d/chessBoard";
 import { Canvas } from "@react-three/fiber";
+import GameStatusPanel from "@/components/GameStatusPanel";
 type GameStatus = "not-started" | "waiting-opponent" | "started";
 
 const GamePage: React.FC = () => {
@@ -60,34 +60,15 @@ const GamePage: React.FC = () => {
 
   return (
     <>
-    <div className="relative  w-full">
-  <div className="absolute top-4 right-4 z-10 flex flex-col items-center bg-white/90 rounded-lg shadow-lg p-4">
-    <div className="flex items-center gap-2">
-      <h1>Game Page</h1>
-      {isConnected ? <div className="bg-green-500 size-3 animate-pulse rounded-full"/> : <div className="bg-red-500 size-3 rounded-full"></div>}
-    </div>
-    {message && <div className="text-red-500">{message}</div>}
-    {gameStatus === "not-started" && (
-      <div>
-        <h2>Please start your game</h2>
-      </div>
-    )}
-    {gameStatus === "waiting-opponent" && (
-      <div>
-        <h2>Waiting for opponent to join...</h2>
-      </div>
-    )}
-    {gameStatus === "started" && playerColor && (
-      <div>
-        <h2>Game is already in progress</h2>
-        <h3>Your color: {playerColor}</h3>
-      </div>
-    )}
-    <Button onClick={handleStartGame} disabled={!isConnected || gameStatus !== "not-started"}>
-      Start Game
-    </Button>
+    <div className="relative w-full min-h-screen bg-gradient-to-br from-indigo-900 via-purple-900 to-pink-800 overflow-hidden">
+    <GameStatusPanel
+      isConnected={isConnected}
+      message={message}
+      gameStatus={gameStatus}
+      playerColor={playerColor}
+      handleStartGame={handleStartGame}
+    />
   </div>
-</div>
     <Canvas
     shadows
     className="bg-linear-to-b from-black to-zinc-700 "
