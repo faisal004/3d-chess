d3d4f4530deec5b1a5c173fdfd4b7969af7175ea
Faisal Husain <faisalhusain1320@gmail.com>
2025-06-22 14:29:18 +0530

    fix : small css issue

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,3 +1,4 @@
+import { Loader } from "lucide-react";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
@@ -5,7 +6,9 @@ const HomePage = React.lazy(() => import("../pages/HomePage"));
 const GamePage = React.lazy(() => import("../pages/GamePage"));
 
 const AppRoutes: React.FC = () => (
-  <React.Suspense fallback={<div>Loading...</div>}>
+  <React.Suspense fallback={<div className="h-screen flex items-center justify-center">
+    <Loader className="animate-spin"/>
+  </div>}>
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/game" element={<GamePage />} />
